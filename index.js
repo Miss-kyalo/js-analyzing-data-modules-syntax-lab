56d81ff22bb0999bb5b776406e3309cbd5ea3e49
Miss-kyalo <bellakyalo@gmail.com>
2026-07-06 10:35:20 +0300

    combined users

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
+function combineUsers(...args){
+const combinedObject = {
+    users: []
+};
+for (const array of args){
+    combinedObject.users.push(...array);
+}
 
+combinedObject.merge_date = "7/6/2026";
+return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
